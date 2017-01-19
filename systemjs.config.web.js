bca321c75a5d1e439cdd44d72480f5f3ac503fe0
Matt Critchlow <mcritch@vmware.com>
2017-01-19 15:31:34 -0800

    Addng dev time dependencies and locking down versions.

--- systemjs.config.web.js
+++ systemjs.config.web.js
@@ -27,22 +27,22 @@
          // our app is within the app folder
          'app': 'app',
          // angular bundles
-         '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-         '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-         '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-         '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-         '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-         '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-         '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-         '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-         '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
-         '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+         '@angular/core': 'npm:@angular/core@2.4.4/bundles/core.umd.js',
+         '@angular/common': 'npm:@angular/common@2.4.4/bundles/common.umd.js',
+         '@angular/compiler': 'npm:@angular/compiler@2.4.4/bundles/compiler.umd.js',
+         '@angular/platform-browser': 'npm:@angular/platform-browser@2.4.4/bundles/platform-browser.umd.js',
+         '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@2.4.4/bundles/platform-browser-dynamic.umd.js',
+         '@angular/http': 'npm:@angular/http@2.4.4/bundles/http.umd.js',
+         '@angular/router': 'npm:@angular/router@3.4.4/bundles/router.umd.js',
+         '@angular/forms': 'npm:@angular/forms@2.4.4/bundles/forms.umd.js',
+         '@angular/upgrade': 'npm:@angular/upgrade@2.4.4/bundles/upgrade.umd.js',
+         '@angular/upgrade/static': 'npm:@angular/upgrade@2.4.4/bundles/upgrade-static.umd.js',
 
          // other libraries
-         'rxjs': 'npm:rxjs',
-         'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-         'ts': 'npm:plugin-typescript@4.0.10/lib/plugin.js',
-         'typescript': 'npm:typescript@2.0.3/lib/typescript.js',
+         'rxjs': 'npm:rxjs@5.0.3',
+         'angular-in-memory-web-api': 'npm:angular-in-memory-web-api@0.2.4/bundles/in-memory-web-api.umd.js',
+         'ts': 'npm:plugin-typescript@5.3.1/lib/plugin.js',
+         'typescript': 'npm:typescript@2.1.5/lib/typescript.js',
 
       },
       // packages tells the System loader how to load when no filename and/or no extension
